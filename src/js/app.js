48c512529b79ca94b1d8613bd6a9e7667b4733c3
cromanalcaide <cromanalcaide@gmail.com>
2022-11-15 19:13:30 +0000

    first (and last?) commit

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,45 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let firstname = `<h1>${variables.name}</h1>`;
+  if (variables.name == null) firstname = "<h1>Your name</h1>";
+  let apellido = `<h1>${variables.lastname}</h1>`;
+  if (variables.lastname == null) apellido = "<h1></h1>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name === null ? "Full " : variables.name} ${
+    variables.lastname === null ? "name" : variables.lastname
+  }</h1>
+          <h2>${
+            variables.role === null ? "4Geeks Student" : variables.role
+          }</h2>
+          <h3>${variables.city === null ? "Barcelona" : variables.city}, ${
+    variables.country === null ? "Spain" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter === null
+                ? "https://twitter.com/4geeksacademy"
+                : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github === "alesanchezr"
+                ? "https://github.com/4geeksacademy"
+                : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.github === null
+                ? "https://linkedin.com/4geeksacademy"
+                : variables.github
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram === null
+                ? " https://instagram.com/4geeksacademy"
+                : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
